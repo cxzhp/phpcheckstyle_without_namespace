aea43b977433cdc01e49b3b24b2b312fb598ce85
zhouping01 <zhouping01@baidu.com>
2016-01-29 22:37:09 +0800

    optimize

--- cs.sh
+++ cs.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 
-git clone https://github.com/cxzhp/phpcheckstyle_without_namespace.git  ._pcs 1>/dev/null 2>&1
+dir='._pcs'
+git clone https://github.com/cxzhp/phpcheckstyle_without_namespace.git  $dir 1>/dev/null 2>&1
 
 run_sh_dir=$(cd `dirname $0`; pwd)
-sh $run_sh_dir/run.sh $1
+sh $run_sh_dir/$dir/run.sh $1
